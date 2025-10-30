afa0cdb221bba39eee5fdeecfd03adc60d4002cb
mustikaweni <mustikamustika492@gmail.com>
2025-10-30 15:24:39 +0700

    tugas1

---
+++ src/main/java/id/ac/unpas/pp2_c_223040139/PP2_C_223040139.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package id.ac.unpas.pp2_c_223040139;
+
+/**
+ *
+ * @author ACER
+ */
+public class PP2_C_223040139 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/id/ac/unpas/pp2_c_223040139/modul05/Latihan4.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.unpas.pp2_c_223040139.modul05;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author ACER
+ */
+public class Latihan4 {
+     public static void main(String[] args){
+        SwingUtilities.invokeLater(new Runnable(){
+        public void run () {
+            JFrame frame = new JFrame ("Contoh BorderLayout");
+                frame.setSize(400, 300);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setLayout(new FlowLayout());
+                
+                JLabel label = new JLabel ("Label ada diAtas (NORTH");
+                JButton button = new JButton("Tombol ada di Bawah (SOUTH)");
+                
+                button.addActionListener(e -> {
+                    label.setText("Tombol di SOUTH diklik");
+                });
+                
+                frame.add(label, BorderLayout.NORTH);
+                frame.add(button, BorderLayout.SOUTH);
+             
+                frame.add(new JButton("WEST"), BorderLayout.WEST);
+                frame.add(new JButton("EAST"), BorderLayout.EAST);
+                frame.add(new JButton("CENTER"), BorderLayout.CENTER);
+                
+                 button.addActionListener(e -> {
+
+                label.setText("Tombol berhasil diklik");
+                
+                 });
+                
+        
+                frame.add(label, BorderLayout.NORTH);
+                frame.add(label, BorderLayout.SOUTH);
+                frame.add(button, BorderLayout.WEST);
+                frame.add(button, BorderLayout.CENTER);
+                
+
+     
+                  
+                frame.setVisible(true);
+     }
+        });       
+}
+}
+
+
+
+
